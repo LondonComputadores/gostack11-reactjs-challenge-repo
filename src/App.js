ff1fa37a4ae78bca238123028f99127ca8b0047f
LondonComputadores <london.computadores@gmail.com>
2020-05-28 19:15:42 -0400

    initial commits

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 import "./styles.css";
+import Header from './components/Header';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories', {
+      title: 'New Repo'
+  });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleUpdateRepository(id) {
+    const response = await api.put('repository/:id', {
+      title: 'Another Repo'
+  });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete('repository/:id', {
+      title: 'New Repository'
+  });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
-  return (
+  return (  
+  
     <div>
       <ul data-testid="repository-list">
         <li>
@@ -25,6 +58,7 @@ function App() {
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
+
   );
 }
 
